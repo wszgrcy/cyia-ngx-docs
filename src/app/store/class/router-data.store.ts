d5460b2e8497263caa985afac4298b23901be543
wszgrcy <wszgrcy@gmail.com>
2020-12-28 09:23:54 +0800

    feat: 请求缓存

--- src/app/store/class/router-data.store.ts
+++ src/app/store/class/router-data.store.ts
@@ -1,10 +1,11 @@
 import { StoreBase } from '../store.types';
 import { NgrxStore } from '../store.decorator';
 import { NgrxAction } from '../action.decorator';
+import { RouterDataEntity } from '@resource-entity/router-data.entity';
 @NgrxStore()
 export class RouterDataStore implements StoreBase<{ [routerLink: string]: any[] }> {
   readonly initState = {};
-  state: { [routerLink: string]: any[] };
+  state: { [routerLink: string]: RouterDataEntity[] };
   @NgrxAction()
   ADD(action: { link: string; list: any[] }) {
     this.state[action.link] = action.list;
--- src/app/store/selector/router-data.selector.ts
+++ src/app/store/selector/router-data.selector.ts
@@ -1,7 +1,11 @@
-import { pipe } from 'rxjs';
+import { pipe, UnaryFunction, Observable } from 'rxjs';
 import { filter } from 'rxjs/operators';
+import { RouterDataEntity } from '@resource-entity/router-data.entity';
 
-export const selectRouterData = pipe(
+export const selectRouterData: UnaryFunction<
+  Observable<{ [routerLink: string]: RouterDataEntity[] }>,
+  Observable<{ [routerLink: string]: RouterDataEntity[] }>
+> = pipe(
   filter((e) => {
     return !!e;
   })
